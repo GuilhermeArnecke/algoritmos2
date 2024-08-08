763a94d01ce1bd7df964176e1ffaf2641e5ab98c
Guilherme <guilhermearnecke@gmail.com>
2024-08-07 21:48:57 -0300

    Estruturado melhor pastas e começado exemplos de aula

---
+++ src/Codes/algoritmo/Format.js
@@ -0,0 +1,8 @@
+let a = 5
+let b = 3
+console.log('divisão simples =' , a / b, 'teste');
+console.log(`divisão simples = ${a / b} teste`);
+
+console.log('divisão fixed =', (a / b).toFixed(2), 'teste');
+console.log(`divisão fixed = ${(a / b).toFixed(3)} teste`);
+console.log(`divisão fixed = ${(a / b).toFixed(4)} teste`);
